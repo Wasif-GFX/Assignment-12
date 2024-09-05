ebba79798e779a8b6fd09b1ceb3d25a4543ce549
Wasif <111518026+Wasif-GFX@users.noreply.github.com>
2024-09-06 01:33:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,85 @@
+// Assignment # 1
+
+// alert("Hello World")
+
+// alert("Error! Please enter a valid password.")
+
+// alert("Welecome to Js \n Happy Codding")
+
+// alert("Welecome to JS Land...")
+
+// alert("Happy Coding! \n Prevent the page from creating additional dialogs")
+
+
+// Assignment # 2
+
+// var fullName = ("Muhammad Wasif")
+
+// var myName = ("Wasif"); 
+
+// var hello = ("Hello World")
+// alert(hello)
+
+// var name = "Muhammad Wasif";
+// alert("Hello," + " " + name + "! Welcome to my website.");
+
+// var pizza = ("PIZZA \n PIZZ \n PIZ \n PI \n p")
+// alert(pizza)
+
+// var mail = "wasifali42401@gmail.com";
+// alert("My Email address is" + " " + mail );
+
+// var name;
+// name = "Muhammad Wasif";
+// alert(name);
+
+// document.write("Yah! I can write HTML content")
+
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// document.write(design);
+// alert(design);
+
+
+// Assignment # 3
+
+// var year;
+// year = 20;
+// var age = "I am" + " " + year + " " + "Years Old "
+// alert(age)
+
+// var N;
+// N = 14;
+// var visited = "You have visited this site" + " " + N + " " + "times."
+// alert(visited)
+
+// var age;
+// age = "2006"
+// var birth = "My Birth Year is" + " " + age
+// document.write(birth)
+
+// var visitorName = "Ahmed"
+// var productTitle = "Sheos"
+// var Quantity = "3"
+// var store = visitorName + " " + "ordered" + " " + Quantity + " " + productTitle + " " + "on Bata Online Store"
+// document.write(store)
+
+
+// Assignment # 4
+
+// var name = "Muhammad Wasif", Fname = "Muhammad Ali", Class = "XII" 
+
+// var My_name_is= "Llegal"
+// var $My_Name_is= "Llegal"
+// var _my_name_is= "Llegal"
+// var MyNameIs= "Llegal"
+// var MyNameIs123= "Llegal"
+
+// var 123MyNameIs= "Illegal"
+// var &My_Name_is= "Illegal"
+// var My Name Is= "Illegal"
+// var @MyNameIs= "Illegal"
+// var ~MyNameIs= "Illegal"
+
+// var content = "Rules for naming JS variables \n \n \n Variable names can only contain, numbers, $ and. For example: $my_1stVariable \n Variable must begin with a letter, Sor. For example: Sname, name or name \n Variable names are case sensitive \n Variable names should not be JS keywords"
+
+// document.write(content)
